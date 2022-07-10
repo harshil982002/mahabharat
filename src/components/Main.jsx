092e65afa4f8d87212e82b6a506c27ac93becb22
harshil982002 <harshilpatel982002@gmail.com>
2022-07-10 21:30:44 +0530

    1st commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import videobg from '../assets/Dharmkshetra.mp4'
+
+const Main=()=> {
+  return (
+    <div>
+      <video src={videobg} autoPlay ></video>
+    </div>
+  )
+}
+
+export default Main
